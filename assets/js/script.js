9c61829fdfd9e5e1e599da9847e9335d5776d610
Mayur Patel <pmayur0680@gmail.com>
2022-04-06 18:20:52 -0500

    updated js for timeblock color changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,37 @@
 // current day is displayed at the top of the calendar
 $("#currentDay").text(moment().format("dddd, MMM Do"));
+
+// fires when the page is loaded 
+function init()
+{
+    for(let i=9; i<=17; i++)
+    {
+        var timeblockid ="hours" + i;
+        var timeblock = document.getElementById(timeblockid);
+        
+        // change color for past, present, future hour    
+        if(moment().hours() > i)
+        {
+            // past hours
+            $(timeblock).addClass("past");
+            $(timeblock).removeClass("future");
+            $(timeblock).removeClass("present");            
+        }
+        else if(moment().hours() < i)
+        {
+            // future hours
+            $(timeblock).addClass("future");
+            $(timeblock).removeClass("past");
+            $(timeblock).removeClass("present");
+        }
+        else
+        {
+            // current hour
+            $(timeblock).addClass("present");
+            $(timeblock).removeClass("past");
+            $(timeblock).removeClass("future");
+        }
+    }
+}
+
+init();
