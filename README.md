31834d45c0914a33636ec35d04e6f7860d389347
Mayur Patel <pmayur0680@gmail.com>
2022-04-06 19:00:31 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# 05-HW-Work-Day-Scheduler
+# Third-Party APIs: Work Day Scheduler
+## About The Project
+This is a home work done by a student of Coding Boot Camp - University of Texas at Austin in April, 2022. The work showcases skill learned: Third-Party APIs: Bootstrap, jQuery and Moment.js. The project allows a user to save events for each hour of the day.
+## License
+Distributed under the MIT License. See LICENSE.txt for more information.
+## Project Links
+Code Repo: https://github.com/pmayur0680/05-HW-Work-Day-Scheduler<br>
+Live Site: 
+## Screenshots
+## Acknowledgments
+ I've included a few of my favorites to lean Third-Party APIs. 
+ - https://getbootstrap.com/docs/4.6/getting-started/introduction/
+ - https://www.w3schools.com/bootstrap4/default.asp
+ - https://momentjs.com/
+ - https://learn.jquery.com/
+ - https://www.w3schools.com/jquery/jquery_selectors.asp
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 // current day is displayed at the top of the calendar
 $("#currentDay").text(moment().format("dddd, MMM Do"));
 
+// jQuery listener for click on save button
+$(".saveBtn").on("click", function(){
+    var textboxid ="txt" + $(this).parent().attr("id");
+    var textbox = document.getElementById(textboxid);
+    // set value to local storage, add date prefix in localstorage variable to make it unique per day
+    var textboxidval = moment().format('L').replace(/\//g, '') + textboxid;
+    localStorage.setItem(textboxidval, $(textbox).val());    
+})
+
 // fires when the page is loaded 
 function init()
 {
@@ -8,7 +17,13 @@ function init()
     {
         var timeblockid ="hours" + i;
         var timeblock = document.getElementById(timeblockid);
-        
+        var textboxidval = "txt" + timeblockid;
+        // add date prefix in localstorage variable to make it unique per day
+        var localstorage = moment().format('L').replace(/\//g, '') + "txt" + timeblockid;
+        var textboxid = document.getElementById(textboxidval);        
+        // get value from local storage        
+        $(textboxid).val(localStorage.getItem(localstorage));
+
         // change color for past, present, future hour    
         if(moment().hours() > i)
         {
